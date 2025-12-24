49165c6b58289c2722991d7752bd9f1cb75ff9c2
rose <zinzinthinzaw@gmail.com>
2025-12-24 09:23:00 +0630

    function

---
+++ chapter7/lib1.js
@@ -0,0 +1,11 @@
+console.log('Lib loaded');
+
+dbconnection();
+
+function api(){
+    console.log('API called');
+}
+
+function dbconnection(){
+    console.log('DB connected successfully...');
+}
---
+++ chapter7/lib2.js
@@ -0,0 +1,17 @@
+window.Module = window.Module || (function () {
+    console.log('Lib loaded');
+
+    dbconnection();
+
+    function api() {
+        console.log('API called');
+    }
+
+    function dbconnection() {
+        console.log('DB connected successfully...');
+    }
+
+    return {
+        api
+    }
+})();
