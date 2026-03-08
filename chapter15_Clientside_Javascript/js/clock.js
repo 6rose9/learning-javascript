17e2ac621364af1a69921b8d6890632552a5860c
rose <zinzinthinzaw8@gmail.com>
2026-03-08 09:41:24 +0630

    callback & IIFE

--- chapter15_Clientside_Javascript/js/clock.js
+++ chapter15_Clientside_Javascript/js/clock.js
@@ -1,8 +1,35 @@
-console.log('clock.js is loaded.');
+// console.log('clock.js is loaded.');
 
-const clockEl = document.getElementById("clock");
+// ------------- if script loaded more than one,
+//               then Identifier 'clockEl' has already been declared ERROR -------------
+// let clockEl = document.getElementById("clock");
 
-setInterval(() => {
-    let time = new Date().toLocaleString();
-    clockEl.innerText = time;
-},1000);
+// setInterval(() => {
+//     let time = new Date().toLocaleString();
+//     clockEl.innerText = time;
+// },1000);
+
+// function api(){
+//     console.log("API called");
+// }
+
+// IIFE module pattern
+window.ClockJS = window.ClockJS || (function () {
+    
+    console.log('clock.js is loaded.');
+
+    let clockEl = document.getElementById("clock");
+
+    setInterval(() => {
+        let time = new Date().toLocaleString();
+        clockEl.innerText = time;
+    }, 1000);
+
+    function api() {
+        console.log("API called");
+    }
+
+    return {
+        api
+    };
+})();
