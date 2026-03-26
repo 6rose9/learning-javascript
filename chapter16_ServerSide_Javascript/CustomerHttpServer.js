a64a90d2c3ddf1d7127297ff13f3ab3ee7a3af77
rose <zinzinthinzaw8@gmail.com>
2026-03-26 08:08:25 +0630

    socket

---
+++ chapter16_ServerSide_Javascript/CustomerHttpServer.js
@@ -0,0 +1,62 @@
+const net = require('node:net');
+// const server = net.createServer(socket => {
+//     console.log('Client connected');
+//     socket.on('data', (data) => {
+//         console.log(`Received from client: ${data.toString()}`);
+//         socket.write(`Echo from server: ${data.toString()}`);
+//     });
+
+//     // End event: called when the client disconnects
+//     socket.on('end', () => {
+//         console.log('Client disconnected');
+//     });
+// });
+
+const server = net.createServer();
+
+function writeResponse(socket, url) {
+    let body = `<html>
+    <h1>Hello from Custom HTTP Server</h1>
+    </html>`;
+
+    // format
+    let response = `HTTP/1.0 200 OK\r\n`
+        + 'Content-Type: text/html\r\n'
+        + 'Connection: Closed\r\n\r\n'
+        + body;
+
+    socket.write(response);
+    socket.end();
+}
+
+server.on('connection', socket => {
+
+    console.log('Client connected');
+
+    socket.on('data', (data) => {
+
+        let request = data.toString();
+        console.log(`Received from client: `);
+
+        let lines = request.split('\r\n');
+        let statusLine = lines[0];
+        let statusParts = statusLine.split(' ');
+        let method = statusParts[0];
+        let url = statusParts[1];
+
+        console.log(`Method: ${method}, URL: ${url}`);
+        console.log(request);
+
+        socket.write(`Echo from server: ${request}`);
+        socket.end();
+    });
+
+    // End event: called when the client disconnects
+    socket.on('end', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
