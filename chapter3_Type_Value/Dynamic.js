b52e71496040b1cc86ec64beabb65d79663c2c9d
rose <zinzinthinzaw8@gmail.com>
2026-03-31 09:09:47 +0630

    typeof

---
+++ chapter3_Type_Value/Dynamic.js
@@ -0,0 +1,15 @@
+let a = 7;
+
+console.log('type of a', typeof a); // number
+
+a = "hello";
+console.log('type of a', typeof a); // string
+
+a = true;
+console.log('type of a', typeof a); //boolean
+
+a = null;
+console.log('type of a', typeof a); // object
+
+a = undefined;
+console.log('type of a', typeof a); // undefined
