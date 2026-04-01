0b1b01a54155cbbce6773a5f417fc4d936f13a01
rose <zinzinthinzaw8@gmail.com>
2026-04-01 08:46:02 +0630

    Number

---
+++ chapter3_Type_Value/POS.js
@@ -0,0 +1,14 @@
+let item = "Apple";
+let qty = 10;
+let price = 15.0;
+
+let total = qty * price;
+console.log(`Total cost of ${qty} ${item} is \$${total}`);
+
+let item1 = {
+    name: "Banana",
+    qty: 5,
+    price: 11.5,
+}
+
+console.log(`Total cost of ${item1.qty} ${item1.name} is \$${item1.qty * item1.price}`);
